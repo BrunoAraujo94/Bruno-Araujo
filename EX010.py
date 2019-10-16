4b55e46e6e6163f75e658ba62b4b6af14ff7bb08
BrunoAraujo94 <55995708+BrunoAraujo94@users.noreply.github.com>
2019-10-16 09:05:12 -0300

    Delete EX010.py

--- EX010.py
+++
@@ -1,7 +0,0 @@
-#CRIE UM PROGRAMA QUE LEIA A QUANTIDADE DE DINHEIRO QUE A
-#PESSOA TEM NA CARTEIRA E MOSTRE QUANTOS DOLARES ELA PODE
-#COMPRAR
-real = float(input("Informe a quantidade de reais para conversão: BRL "))
-cotacao = float(input('Informe a cotação do Dolar : '))
-conversao = real/cotacao
-print('A quantidade de dólar convertido em real é: R$',conversao)
