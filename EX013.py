4357699cd280ed89f4bb7e3efc7112039acdccad
BrunoAraujo94 <55995708+BrunoAraujo94@users.noreply.github.com>
2019-10-16 08:55:20 -0300

    Delete EX013.py

--- EX013.py
+++
@@ -1,9 +0,0 @@
-#Faça um algoritimo que leia o salário de um funcionário e mostre seu novo salario
-#com 15% de aumento
-S = float(input('Digite o salário : '))
-A = S*1.15
-print('O salário de {} com o reajuste de 15% será {}'.format(S,A))
-
-x = 'Always take a banana to a party!'
-print([x-3])
-
