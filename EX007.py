95308ad7d93b933705902be12db0b873d1bcd439
BrunoAraujo94 <55995708+BrunoAraujo94@users.noreply.github.com>
2019-10-16 08:54:00 -0300

    Delete EX007.py

--- EX007.py
+++
@@ -1,9 +0,0 @@
-#DESENVOLVA UM PROGRAMA QUE LEIA AS DUAS NOTAS DE UM ALUNO
-#CALCULE E MOSTRE A SUA MÉDIA
-n1 = int(input('Nota do primeiro semestre'))
-n2 = int(input('nota do segundo semestre'))
-n3 = int(input('nota do terceiro semestre'))
-n4 = int(input('nota do quarto semestre'))
-soma = n1+n2+n3+n4
-média = soma/4
-print('A média final do aluno é ', média)
