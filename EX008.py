d342d3e442ca7417b91b679f6c973f078e1eb903
BrunoAraujo94 <55995708+BrunoAraujo94@users.noreply.github.com>
2019-10-16 09:04:49 -0300

    Delete EX008.py

--- EX008.py
+++
@@ -1,6 +0,0 @@
-#ESCREVA UM PROGRAMA QUE LEIA UM VALOR EM METROS E O
-#EXIBA CONVERTIDO EM CENTIMETROS E MILIMETROS
-n1 = int(input('Digite os metros: '))
-metros = n1*100
-milimetros = n1*1000
-print('O total de centímetros é {}, e o total de milimentos é {}'.format(metros,milimetros))
